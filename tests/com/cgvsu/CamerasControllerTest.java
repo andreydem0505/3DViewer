184a52bf2c85d42a78ec67724e70370d36f79520
Andrey <andreydem42@gmail.com>
2024-12-12 15:14:38 +0300

    fix camera controller tests

--- tests/com/cgvsu/CamerasControllerTest.java
+++ tests/com/cgvsu/CamerasControllerTest.java
@@ -1,5 +1,7 @@
 package com.cgvsu;
 
+import com.cgvsu.math.Linal;
+import com.cgvsu.nmath.Vector2f;
 import com.cgvsu.nmath.Vector3f;
 import com.cgvsu.render_engine.Camera;
 import org.junit.jupiter.api.Assertions;
@@ -8,12 +10,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CamerasControllerTest {
+    private Camera getCamera() {
+        return new Camera(
+                new Vector2f(Linal.pi / 4, Linal.pi / 4),
+                100f,
+                new Vector3f(0, 0, 0),
+                1.0F,
+                1,
+                0.01F,
+                100);
+    }
+
     @Test
     public void testInitialization() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
         Assertions.assertEquals(camera, controller.currentCamera);
         Assertions.assertEquals(1, controller.getCamerasQuantity());
@@ -21,30 +31,18 @@ public class CamerasControllerTest {
 
     @Test
     public void testCameraAddition() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        controller.addCamera(new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100));
+        controller.addCamera(getCamera());
         Assertions.assertEquals(camera, controller.currentCamera);
         Assertions.assertEquals(2, controller.getCamerasQuantity());
     }
 
     @Test
     public void testSetCurrent() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        camera = getCamera();
         controller.addCamera(camera);
         controller.setCurrent(1);
         Assertions.assertEquals(camera, controller.currentCamera);
@@ -53,30 +51,18 @@ public class CamerasControllerTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 2})
     public void testSetCurrentIncorrectValue(int number) {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        controller.addCamera(new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100));
+        controller.addCamera(getCamera());
         controller.setCurrent(number);
         Assertions.assertEquals(camera, controller.currentCamera);
     }
 
     @Test
     public void testRemoveCamera() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        controller.addCamera(new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100));
+        controller.addCamera(getCamera());
         controller.removeCamera(1);
         Assertions.assertEquals(camera, controller.currentCamera);
         Assertions.assertEquals(1, controller.getCamerasQuantity());
@@ -84,15 +70,9 @@ public class CamerasControllerTest {
 
     @Test
     public void testRemoveCurrentCamera() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        controller.addCamera(new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100));
+        controller.addCamera(getCamera());
         controller.setCurrent(1);
         controller.removeCamera(1);
         Assertions.assertEquals(camera, controller.currentCamera);
@@ -102,15 +82,9 @@ public class CamerasControllerTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 2})
     public void testRemoveWithIncorrectIndex(int number) {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
-        controller.addCamera(new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100));
+        controller.addCamera(getCamera());
         controller.removeCamera(number);
         Assertions.assertEquals(camera, controller.currentCamera);
         Assertions.assertEquals(2, controller.getCamerasQuantity());
@@ -118,10 +92,7 @@ public class CamerasControllerTest {
 
     @Test
     public void testRemoveTheOnlyCamera() {
-        Camera camera = new Camera(
-                new Vector3f(0, 0, 100),
-                new Vector3f(0, 0, 0),
-                1.0F, 1, 0.01F, 100);
+        Camera camera = getCamera();
         CamerasController controller = new CamerasController(camera);
         controller.removeCamera(0);
         Assertions.assertEquals(camera, controller.currentCamera);
