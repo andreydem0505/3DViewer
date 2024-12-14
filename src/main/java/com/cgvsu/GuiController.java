5fb2ffa53cf60546c10e4ed4dd311338f3b1c3cb
lein3000zzz <146432991+lein3000zzz@users.noreply.github.com>
2024-12-15 00:10:09 +0300

    Connected interface and camera controls

--- src/main/java/com/cgvsu/GuiController.java
+++ src/main/java/com/cgvsu/GuiController.java
@@ -13,11 +13,16 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
 import java.nio.file.Files;
@@ -32,6 +37,27 @@ import com.cgvsu.render_engine.Camera;
 public class GuiController {
 
     final private float TRANSLATION = 2F;
+
+    @FXML
+    private TreeView<String> camerasTree;
+
+    @FXML
+    private TextField directionX;
+
+    @FXML
+    private TextField directionY;
+
+    @FXML
+    private TextField directionZ;
+
+    @FXML
+    private TextField cameraX;
+
+    @FXML
+    private TextField cameraY;
+
+    @FXML
+    private TextField distance;
 
     @FXML
     AnchorPane anchorPane;
@@ -65,6 +91,12 @@ public class GuiController {
         PixelWriter pixelWriter = new PixelWriter(imageView);
 
         RenderEngine renderEngine = new RenderEngine();
+
+        if (camerasController.getCamerasQuantity() == 1) {
+            updateCameraTree();
+            setCurrent(0);
+            System.out.println("lol");
+        }
 
         KeyFrame frame = new KeyFrame(Duration.millis(30), event -> {
             double width = imageView.getFitWidth();
@@ -128,31 +160,113 @@ public class GuiController {
     @FXML
     public void handleCameraForward(ActionEvent actionEvent) {
         camerasController.currentCamera.moveDistance(-TRANSLATION);
+        updateFields();
     }
 
     @FXML
     public void handleCameraBackward(ActionEvent actionEvent) {
         camerasController.currentCamera.moveDistance(TRANSLATION);
+        updateFields();
     }
 
     @FXML
     public void handleCameraLeft(ActionEvent actionEvent) {
         camerasController.currentCamera.moveRotation(new Vector2f(TRANSLATION / 100, 0));
+        updateFields();
     }
 
     @FXML
     public void handleCameraRight(ActionEvent actionEvent) {
         camerasController.currentCamera.moveRotation(new Vector2f(-TRANSLATION / 100, 0));
-
+        updateFields();
     }
 
     @FXML
     public void handleCameraUp(ActionEvent actionEvent) {
         camerasController.currentCamera.moveRotation(new Vector2f(0, -TRANSLATION / 100));
+        updateFields();
     }
 
     @FXML
     public void handleCameraDown(ActionEvent actionEvent) {
         camerasController.currentCamera.moveRotation(new Vector2f(0, TRANSLATION / 100));
+        updateFields();
+    }
+
+    @FXML
+    private void addCamera() {
+        camerasController.addCamera(
+                new Camera(
+                        new Vector2f(Linal.pi / 4, Linal.pi / 4),
+                        100f,
+                        new Vector3f(0, 0, 0),
+                        1.0F,
+                        1,
+                        0.01F,
+                        100)
+        );
+        updateCameraTree();
+    }
+
+    private void setCurrent(int index) {
+        camerasController.setCurrent(index);
+        camerasTree.getSelectionModel().select(index);
+        updateFields();
+    }
+
+    @FXML
+    private void removeCamera() {
+        if (camerasController.getCamerasQuantity() == 1) {
+            showAlert("Cringe", "Нельзя удалить последнюю камеру");
+            return;
+        }
+        camerasController.removeCamera(camerasTree.getSelectionModel().getSelectedIndex());
+        updateCameraTree();
+    }
+
+    private void updateFields() {
+        distance.setText(String.valueOf(camerasController.currentCamera.getDistance()));
+
+        cameraX.setText(String.valueOf(camerasController.currentCamera.getRotation().x()));
+        cameraY.setText(String.valueOf(camerasController.currentCamera.getRotation().y()));
+
+        directionX.setText(String.valueOf(camerasController.currentCamera.getTarget().x()));
+        directionY.setText(String.valueOf(camerasController.currentCamera.getTarget().y()));
+        directionZ.setText(String.valueOf(camerasController.currentCamera.getTarget().y()));
+    }
+
+    private void updateCameraTree() {
+        TreeItem<String> root = new TreeItem<>("Cameras");
+        camerasTree.setRoot(root);
+        for (int i = 0; i < camerasController.getCamerasQuantity(); i++) {
+            root.getChildren().add(new TreeItem<>("Camera " + (i + 1)));
+        }
+        camerasTree.setShowRoot(false);
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void handleCameraSelection(MouseEvent event) {
+        TreeItem<String> selectedItem = camerasTree.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            int selectedIndex = camerasTree.getRoot().getChildren().indexOf(selectedItem);
+            if (selectedIndex >= 0) {
+                setCurrent(selectedIndex);
+            }
+        }
+    }
+
+    @FXML
+    private void updateCameraDirectionAndPosition() {
+        camerasController.currentCamera.setTarget(new Vector3f(Float.parseFloat(directionX.getText()), Float.parseFloat(directionX.getText()), Float.parseFloat(directionX.getText())));
+        camerasController.currentCamera.setDistance(Float.parseFloat(distance.getText()));
+        camerasController.currentCamera.setRotation(new Vector2f(Float.parseFloat(cameraX.getText()), Float.parseFloat(cameraY.getText())));
+//        currentCamera.setPosition(parseVector(cameraX.getText(), cameraY.getText(), cameraZ.getText()));
     }
 }
