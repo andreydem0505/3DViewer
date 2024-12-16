13e7370bb77bb6e88c45e220f4c6d01ef6fad9eb
lein3000zzz <146432991+lein3000zzz@users.noreply.github.com>
2024-12-16 17:42:42 +0300

    VertexRemoverTest

---
+++ tests/com/cgvsu/model_modification/VertexRemoverNextGenTest.java
@@ -0,0 +1,118 @@
+package com.cgvsu.model_modification;
+
+import com.cgvsu.io.objreader.ObjReader;
+import com.cgvsu.io.objwriter.ObjWriter;
+import com.cgvsu.model.Model;
+import com.cgvsu.model.Polygon;
+import com.cgvsu.nmath.Vector2f;
+import com.cgvsu.nmath.Vector3f;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class VertexRemoverNextGenTest {
+
+    @Test
+    void processModelRemoveAll() {
+        String separator = System.lineSeparator();
+
+        String inputFile = """
+                v 1.0 1.0 1.0
+                v 2.0 2.0 2.0
+                v 3.0 3.0 3.0
+                vt 0.1 0.2
+                vt 0.3 0.4
+                vn 0.0 0.0 1.0
+                vn 1.0 0.0 0.0
+                f 1/1/1 2/2/2 3/1/1
+                f 2/2/2 3/1/1 1/1/1
+                """;
+        String expectedOutput = (
+                "").trim();
+        Model inputModel = ObjReader.read(inputFile);
+
+        VertexRemoverNextGen.processModel(inputModel, List.of(0, 1, 2), true, false, true, true, false);
+
+        String inputRes = ObjWriter.formatOutput(inputModel, System.lineSeparator());
+        Assertions.assertEquals(expectedOutput, inputRes);
+    }
+
+    @Test
+    void processModelKeepVtAndVn() {
+        String separator = System.lineSeparator();
+
+        String inputFile = """
+                v 1.0 1.0 1.0
+                v 2.0 2.0 2.0
+                v 3.0 3.0 3.0
+                vt 0.1 0.2
+                vt 0.3 0.4
+                vn 0.0 0.0 1.0
+                vn 1.0 0.0 0.0
+                f 1/1/1 2/2/2 3/1/1
+                f 2/2/2 3/1/1 1/1/1
+                """;
+        String expectedOutput = (
+                "vt 0.1 0.2" + separator +
+                        "vt 0.3 0.4" + separator +
+                        "vn 0.0 0.0 1.0" + separator +
+                        "vn 1.0 0.0 0.0").trim();
+        Model inputModel = ObjReader.read(inputFile);
+
+        VertexRemoverNextGen.processModel(inputModel, List.of(0, 1, 2), false, true, false, false, false);
+
+        String inputRes = ObjWriter.formatOutput(inputModel, System.lineSeparator());
+        Assertions.assertEquals(expectedOutput, inputRes);
+    }
+    @Test
+    void processModelKeepVn() {
+        String separator = System.lineSeparator();
+
+        String inputFile = """
+                v 1.0 1.0 1.0
+                v 2.0 2.0 2.0
+                v 3.0 3.0 3.0
+                vt 0.1 0.2
+                vt 0.3 0.4
+                vn 0.0 0.0 1.0
+                vn 1.0 0.0 0.0
+                f 1/1/1 2/2/2 3/1/1
+                f 2/2/2 3/1/1 1/1/1
+                """;
+        String expectedOutput = (
+                "vn 0.0 0.0 1.0" + separator +
+                        "vn 1.0 0.0 0.0").trim();
+        Model inputModel = ObjReader.read(inputFile);
+
+        VertexRemoverNextGen.processModel(inputModel, List.of(0, 1, 2), false, true, false, true, false);
+
+        String inputRes = ObjWriter.formatOutput(inputModel, System.lineSeparator());
+        Assertions.assertEquals(expectedOutput, inputRes);
+    }
+    @Test
+    void processModelKeepVt() {
+        String separator = System.lineSeparator();
+
+        String inputFile = """
+                v 1.0 1.0 1.0
+                v 2.0 2.0 2.0
+                v 3.0 3.0 3.0
+                vt 0.1 0.2
+                vt 0.3 0.4
+                vn 0.0 0.0 1.0
+                vn 1.0 0.0 0.0
+                f 1/1/1 2/2/2 3/1/1
+                f 2/2/2 3/1/1 1/1/1
+                """;
+        String expectedOutput = (
+                "vt 0.1 0.2" + separator +
+                        "vt 0.3 0.4").trim();
+        Model inputModel = ObjReader.read(inputFile);
+
+        VertexRemoverNextGen.processModel(inputModel, List.of(0, 1, 2), false, true, true, false, false);
+
+        String inputRes = ObjWriter.formatOutput(inputModel, System.lineSeparator());
+        Assertions.assertEquals(expectedOutput, inputRes);
+    }
+}
