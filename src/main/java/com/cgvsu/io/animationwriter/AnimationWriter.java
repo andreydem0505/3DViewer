81a678a610b038399376742451919fdf77c7e0b2
Andrey <andreydem42@gmail.com>
2024-12-24 14:47:55 +0300

    gson line

--- src/main/java/com/cgvsu/io/animationwriter/AnimationWriter.java
+++ src/main/java/com/cgvsu/io/animationwriter/AnimationWriter.java
@@ -1,4 +1,9 @@
 package com.cgvsu.io.animationwriter;
 
+import com.google.gson.Gson;
+
 public class AnimationWriter {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+    }
 }
